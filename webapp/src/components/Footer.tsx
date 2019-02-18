4a6b3fe71c8037c5bdfd0e867fce9d98fc0bbd82
Tommi Grönlund <tomgron@calm.fi>
2019-02-18 23:47:01 +0200

    reset web app

--- webapp/src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "./Footer.scss";
-
-export default class Footer extends Component<any, any> {
-  render() {
-    return <div className="footer-container">FOOTER COMPONENT</div>;
-  }
-}
